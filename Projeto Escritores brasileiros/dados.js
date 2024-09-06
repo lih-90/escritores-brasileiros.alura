03c1e68e96ae8d062028072ae7fb191376702bb6
Aline Ometto <aline.ometto90@gmail.com>
2024-09-06 19:44:46 -0300

    Update dados.js

--- Projeto Escritores brasileiros/dados.js
+++ Projeto Escritores brasileiros/dados.js
@@ -3,43 +3,48 @@ let dados = [{
     descricao: "Machado de Assis: Considerado um dos maiores escritores brasileiros, Machado de Assis dominou o Realismo e o Naturalismo com obras como Dom Casmurro, Memórias Póstumas de Brás Cubas e Quincas Borba. Sua ironia e profundidade psicológica o tornaram um dos autores mais estudados e admirados no Brasil.",
     obras: "Livros: Dom Casmurro, Memórias Póstumas de Brás Cubas, Quincas Borba, entre outros.",
     link: "https://www.ebiografia.com/machado_assis/",
+    tags: "escritor dom casmurro memórias póstumas memorias postumas"
 },
 {
     nome: "Clarice Lispector",
     descricao: "Clarice Lispector: Uma das maiores escritoras brasileiras, Clarice Lispector explorou a interioridade humana com grande profundidade em obras como A Hora da Estrela e A Paixão Segundo G.H.",
     obras: "Livros: A Hora da Estrela, A Paixão Segundo G.H., A Cidade Sitiada, entre outros.",
     link: "https://www.ebiografia.com/clarice_lispector/",
+    tags: "clarice, estrela, paixão, paixao, GH, cidade"
 },
 {
     nome: "Graciliano Ramos",
     descricao: "Graciliano Ramos: Autor regionalista, Graciliano Ramos retratou a vida no sertão nordestino com grande realismo e profundidade psicológica em obras como Vidas Secas e São Bernardo.",
     obras: "Livros: Vidas Secas, São Bernardo, Angústia, entre outros.",
     link: "https://www.ebiografia.com/graciliano_ramos/",
+    tags: "graciliano vidas secas angustia sao bernardo"
 },
 {
     nome: "Guimarães Rosa",
     descricao: "Guimarães Rosa: Conhecido por sua linguagem rica e inovadora, Guimarães Rosa criou um universo próprio em suas obras, como Grande Sertão: Veredas e Sagarana.",
     obras: "Livros: Grande Sertão: Veredas, Sagarana, Primeiras Estórias, entre outros.",
     link: "https://www.ebiografia.com/guimaraes_rosa/",
+    tags: "guimaraes grande sertão grande sertao primeiras estorias"
 },
 {
     nome: "Carlos Drummond de Andrade",
     descricao: "Carlos Drummond de Andrade: Um dos maiores poetas brasileiros do século XX, Drummond abordou temas como a vida, a morte, o amor e a política com grande sensibilidade e lirismo em obras como Alguma Poesia e Sentimento do Mundo.",
     obras: "Livros: Alguma Poesia, Sentimento do Mundo, Claro Enigma, entre outros.",
     link: "https://www.ebiografia.com/carlos_drummond_de_andrade/",
+    tags: "drummond poesia sentimento claro enigma"
 },
 {
     nome: "Aluísio Azevedo",
     descricao: "Aluísio Azevedo: Um dos principais representantes do Naturalismo no Brasil, Aluísio Azevedo retratou a vida nas grandes cidades e a realidade social de forma crua e realista em obras como O Cortiço e Casa Grande & Senzala.",
     obras: "Livros: O Cortiço, Casa Grande & Senzala, O Mulato, entre outros.",
     link: "https://www.ebiografia.com/aluisio_azevedo/",
+    tags: "aluisio cortiço cortico casa grande mulato"
 },
 {
     nome: "Cecília Meireles",
     descricao: "Cecília Meireles: Poeta lírica e musical, Cecília Meireles abordou temas como a natureza, o tempo e a espiritualidade em obras como Romanceiro da Inconfidência e Viagem.",
     obras: "Livros: Romanceiro da Inconfidência, Viagem, Moema, entre outros.",
     link: "https://www.ebiografia.com/cecilia_meireles/",
+    tags: "cecilia romanceiro romancero inconfidencia poema poemas"
 }
 ];
-
-
